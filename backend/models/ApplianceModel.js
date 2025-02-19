273f85f17121d0617c50bf5b05f8a0580c55b4a1
Keizan Sato <ksato@uwm.edu>
2025-02-19 14:32:09 -0600

    Appliance Model, validation methods

---
+++ backend/models/ApplianceModel.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+class UserModel {
+    static schema = Joi.object({
+        ownerUsername: Joi.string().max(24).require(),
+        name: Joi.string().max(64).require(),
+        description: Joi.string().require(),
+        timeAvailable: Joi.date().timestamp(),
+        lendTo: Joi.string().allow("Same Floor", "Same Building", "Anyone").require(),
+        isVisible: Joi.boolean().require(),
+        created: Joi.date().require()
+    });
+
+    static validate(object) {
+        const {error, value} = schema.validate(object);
+        if (error) {
+            return error;
+        } else {
+            return value;
+        }
+    };
+};
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -3,20 +3,29 @@ const Joi = require('joi');
 class UserModel {
     static schema = Joi.object({
         username: Joi.string().max(24).require(), // Max 24 chars, we can change this
-        email: Joi.string().email().require(),
+        email: Joi.string().email({tlds: {allow: ['edu']}}).require(),
         location: Joi.string().valid("Sandburg East", "Sandburg North", "Sandburg South", "Sandburg West").require(), // Just Sandburg for now
-        floor: Joi.number().greater(0).require().when('location', { // Different max floor depending on building
+        floor: Joi.number().greater(0).when('location', { // Different max floor depending on building
     						    switch: [
                                       {is: 'Sandburg East', then: Joi.number().less(17)},
                                       {is: 'Sandburg North', then: Joi.number().less(26)},
                                       {is: 'Sandburg South', then: Joi.number().less(20)},
                                       {is: 'Sandburg West', then: Joi.number().less(16)}
-                                      ]}),
-        // profilePicture: NOT IMPLEMENTED YET
+                                      ]}).require(),
+        // profilePicture: NOT IMPLEMENTED YET- we can just have default pictures for now since pfps require firebase cloud storage
         created: Joi.date().require(),
         isActive: Joi.boolean(),
         showDorm: Joi.boolean(),
         showFloor: Joi.boolean(),
-        // securityQuestions: NOT IMPLEMENTED YET
+        // securityQuestions: NOT IMPLEMENTED YET- can it be stored with the auth stuff?
     })
+
+    static validate(object) {
+        const {error, value} = schema.validate(object);
+        if (error) {
+            return error;
+        } else {
+            return value;
+        }
+    };
 }
