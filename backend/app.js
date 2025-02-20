12191a6a220fe033ab86e026fdf13851a0bc8ff9
vasiliykravtsov <vasiliy@uwm.edu>
2025-02-19 23:06:22 -0600

    changes

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const applianceRoute = require('./routes/appliance');
 app.use('/login', loginRoute);
 
 app.use('/profile', profileRoute);
-app.use('/routes/appliances', applianceRoute);
+app.use('/routes/appliance', applianceRoute);
 
 app.use((req, res, next) => {
     res.status(200).json({
