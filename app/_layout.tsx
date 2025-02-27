dc467387520719a9b46b9c14bc571b6a3a10320e
FSarkar2023 <113079156+FSarkar2023@users.noreply.github.com>
2025-02-26 19:24:37 -0600

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,46 @@
-// app/_layout.tsx
-import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        <Drawer.Screen
+          name="index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'Home',
+          }}
+        />
+        <Drawer.Screen
+          name="profile_page" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'Profile Page',
+            title: 'Profile Page',
+          }}
+        />
+        <Drawer.Screen
+          name="create_profile"
+          options={{
+            drawerLabel: '',
+            title: 'Create Profile',
+            swipeEnabled: false,
+          }}
+        />
+        <Drawer.Screen
+          name="firebase/firebaseConfig" 
+          options={{
+            drawerLabel: '',
+            swipeEnabled: false,
+          }}
+        />
+        <Drawer.Screen
+          name="+not-found" 
+          options={{
+            drawerLabel: '',
+            swipeEnabled: false,
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
 }
