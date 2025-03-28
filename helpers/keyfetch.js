ef3177b7466e2bdf9532fcc221736a49dc7e2e75
Keizan Sato <ksato@uwm.edu>
2025-03-28 01:03:15 -0500

    Updated packages

--- helpers/keyfetch.js
+++ helpers/keyfetch.js
@@ -4,17 +4,18 @@ import * as SecureStore from 'expo-secure-store';
 
 async function saveInStore(key, value) {
     if (Platform.OS == 'web') {
-        await AsyncStorage.setItem(key, value); // secure store not available on web so we use async store for testing
+        await AsyncStorage.setItem(key, value); // secure store not available on web so we use async store for testing- NOT SECURE
     } else {
         await SecureStore.setItemAsync(key, value.toString());
     }
 }
 
 async function getValue(key) {
+    let result;
     if (Platform.OS == 'web') {
-        const result = await AsyncStorage.getItem(key);
+        result = await AsyncStorage.getItem(key); // Same deal down here
     } else {
-        const result = await SecureStore.getItemAsync(key);
+        result = await SecureStore.getItemAsync(key);
     }
 
     return result;
