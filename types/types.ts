fbdc0d5da2d77cbffe43e46577bbb39943ba25e6
vasiliykravtsov <vasiliy@uwm.edu>
2025-04-04 07:51:23 -0500

    appliance board now interacts with backend for display and creation

--- types/types.ts
+++ types/types.ts
@@ -7,12 +7,10 @@ export type Request = {
 };
 
 export type Appliance = {
-    id: string; // String because request ID is also a string
     ownerUsername: string;
-    Name: string;
-    Description: string;
+    name: string;
+    description: string;
     timeAvailable: Number;
     lendTo: 'Same Floor' | 'Same Dorm' | 'Anyone'
     isVisible: boolean;
-    created: string;
 }
